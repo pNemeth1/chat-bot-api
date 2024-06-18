59d411e72eba8347d6a117a6b261a85f0c3fdefa
MadPad <pasenemeth@gmail.com>
2024-06-18 09:33:25 +0200

    test console

--- src/prompts/prompts.controller.ts
+++ src/prompts/prompts.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { PromptsService } from './prompts.service';
-import { CreatePromptDto } from './dto/create-prompt.dto';
-import { UpdatePromptDto } from './dto/update-prompt.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from "@nestjs/common";
+import { PromptsService } from "./prompts.service";
+import { CreatePromptDto } from "./dto/create-prompt.dto";
+import { UpdatePromptDto } from "./dto/update-prompt.dto";
 
-@Controller('prompts')
+@Controller("prompts")
 export class PromptsController {
   constructor(private readonly promptsService: PromptsService) {}
 
   @Post()
   create(@Body() createPromptDto: CreatePromptDto) {
+    console.log(createPromptDto);
     return this.promptsService.create(createPromptDto);
   }
 
@@ -17,18 +26,18 @@ export class PromptsController {
     return this.promptsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.promptsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePromptDto: UpdatePromptDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updatePromptDto: UpdatePromptDto) {
     return this.promptsService.update(+id, updatePromptDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.promptsService.remove(+id);
   }
 }
