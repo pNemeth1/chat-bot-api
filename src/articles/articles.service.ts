b20ec6cc5829e5d37eb613a35f20d94af94865fd
PNemeth1 <pasenemeth@gmail.com>
2024-06-18 08:54:17 +0200

    initial commit

---
+++ src/articles/articles.service.ts
@@ -0,0 +1,42 @@
+import { Get, Injectable } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class ArticlesService {
+  constructor(private prismaService: PrismaService) {}
+
+  create(createArticleDto: CreateArticleDto) {
+    return this.prismaService.article.create({ data: createArticleDto });
+  }
+
+  @Get()
+  findAll() {
+    return this.prismaService.article.findMany({ where: { published: true } });
+  }
+
+  findOne(id: number) {
+    return this.prismaService.article.findUnique({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
+  }
+
+  update(id: number, updateArticleDto: UpdateArticleDto) {
+    return this.prismaService.article.update({
+      where: { id },
+      data: updateArticleDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.article.delete({ where: { id } });
+  }
+
+  findDrafts() {
+    return this.prismaService.article.findMany({ where: { published: false } });
+  }
+}
